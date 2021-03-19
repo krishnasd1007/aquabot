fdf694bec636a5c0075a57e225558bfaf177bfa0
krishnasd1007 <68434458+krishnasd1007@users.noreply.github.com>
2021-03-19 10:47:22 +0530

    with firebase

--- gps.ino
+++ gps.ino
@@ -1,13 +1,20 @@
 #include <TinyGPS++.h> 
 #include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+
 TinyGPSPlus gps; 
 int RX = 4;
 int TX = 5;
 SoftwareSerial ss(RX, TX); 
 const char* ssid = "Haunted House"; 
 const char* password = "kotha103"; 
+const char* firebase_host = "";
+const char* firebase_auth = "";
 float latitude, longitude, alt;
+
+FirebaseData fbdo;
+String path = "/forward";
 
 void setup() {
   Serial.begin(115200);
@@ -23,11 +30,21 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected");
+  
+  Firebase.begin(firebase_host, firebase_auth);
+  Firebase.reconnectWiFi(true);
+
+  Firebase.beginStream(fbdo, path);
+
+  if (!Firebase.beginStream(fbdo, path))
+  {
+    Serial.println("error beginning stream");
+    Serial.println("reason " + fbdo.errorReason());
+  }
 }
 
 void loop() {
   while (ss.available() > 0) {
-    Serial.print("Hi");
     if (gps.encode(ss.read())) 
     {
       if (gps.location.isValid()) 
@@ -41,6 +58,7 @@ void loop() {
         Serial.println(longitude);
         Serial.print("altitude : ");
         Serial.println(alt);
+        Firebase.setFloat("latitude : ",latitude);
       }
     }
   }
